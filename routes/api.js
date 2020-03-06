5534f3b8d04abf4406066b2b987e404b240fe48c
SuperCheese21 <ethan.shields21@gmail.com>
2020-03-05 18:41:35 -0800

    Added time range filter for objects

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,18 @@ api.get('/cameras', async (req, res) => {
 });
 
 api.get('/objects', async (req, res) => {
-  const objects = await global.ObjectModel.find();
+  let objects = await global.ObjectModel.find();
+
+  const start = req.query.start;
+  if (start) {
+    objects = objects.filter(object => object.timestamp >= Number(start));
+  }
+
+  const end = req.query.end;
+  if (end) {
+    objects = objects.filter(object => object.timestamp <= Number(end));
+  }
+
   res.json(objects);
 });
 
